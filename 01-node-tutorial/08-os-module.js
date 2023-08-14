ba9dea9afdac9b775028808e1f98dae4fadad98d
moneebullah25 <moneebullah25@gmail.com>
2023-08-14 15:18:09 +0500

    Initial commit

---
+++ 01-node-tutorial/08-os-module.js
@@ -0,0 +1,18 @@
+// https://nodejs.org/dist/latest-v18.x/docs/api/os.html
+
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+console.log(currentOS)
